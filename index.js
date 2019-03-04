4df7a5779bbbc4bec31fdf84d355c55fad05603a
Vladyslav Prykhodskyi <v.prihodski@gmail.com>
2019-03-04 16:28:40 +0000

    fib

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-const a = parseInt(prompt('Enter the a value'));
-const b = parseInt(prompt('Enter the b value'));
-const c = parseInt(prompt('Enter the c value'));
-const result = solutionIs(a,b,c);
-function calcDiscr(a,b,c){
-    return b*b - 4*a*c;
+const a = parseInt(prompt("Enter the number: "));
+const resultSum = fibSum(a);
+const resultArray = fibArray(a);
+const resultRecurse = fibRecurse(a);
+alert("Here is Fibonacci number by sum: " + resultSum);
+alert("Here is Fibonacci number by array: " + resultArray);
+alert("Here is Fibonacci number by recurse: " + resultRecurse);
+function fibSum (a) {
+    let fibFirst = 1;
+    let fibSecond = 1;
+    for (let i = 3; i <= a; i++){
+        let fibNext = fibFirst + fibSecond;
+        fibFirst = fibSecond;
+        fibSecond = fibNext;
+    }
+    return fibSecond;
 }
-function solutionIs(a,b,c){
-    const d = calcDiscr(a,b,c);
-    if (d < 0){
-        alert("The equation ain`t have  roots");
-    } 
-    if (d == 0){
-        const x = -b/(2*a);
-        alert("The root is " + x);
+function fibRecurse(a) {
+    if (a <= 1){
+        return a;
     }
-    if(d > 0){
-        const xRoot1 =(-b - Math.sqrt(d))/(2*a);
-        const xRoot2 =(-b + Math.sqrt(d))/(2*a);
-        alert("These are the solution " +xRoot1 +"; " +xRoot2);
+    return fibRecurse(a - 1) + fibRecurse(a - 2);
+}
+function fibArray(a) {
+    let numberArray = [1 , 1];
+    for (let i = 3; i <= a; i++) {
+        let fibNext = numberArray[numberArray.length - 1] + numberArray[numberArray.length - 2];
+        numberArray.push(fibNext);
     }
+    return numberArray[numberArray.length - 1];
 }
